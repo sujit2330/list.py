f1e9177b846d91a64b8a9932804b80902ab6e5f7
sujit2330 <85396740+sujit2330@users.noreply.github.com>
2021-06-12 19:46:55 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+myList1 = [12,-14,34,-56,64]
+myList2 = [12,14,-50,32,-78]
+for num in myList1 :
+    if num >= 0:
+     print(num, end = " ")
+print("\n")
+for num1 in myList2 :     
+    if num1 >=0:
+     print(num1,end =" ")
